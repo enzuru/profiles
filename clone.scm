6d3c10ef0b541fd4490be9414dfb607002b2e402
Ahmed Khanzada <me@enzu.ru>
2021-11-13 12:04:07 -0500

    Clone org notes

--- clone.scm
+++ clone.scm
@@ -2,3 +2,4 @@
 
 (clone "https://github.com/enzuru/home.git" "/home/enzuru")
 (clone "https://github.com/enzuru/.emacs.d.git" "/home/enzuru/.emacs.d")
+(clone "https://github.com/enzuru/org.git" "/home/enzuru/org")
