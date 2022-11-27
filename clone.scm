b4bc799b02c7652877e9f69e8fe6ad780208e49f
Ahmed Khanzada <me@enzu.ru>
2022-11-27 15:27:05 -0500

    Fixing up cloning process

--- clone.scm
+++ clone.scm
@@ -1,5 +1,6 @@
 (use-modules (git))
 
-(clone "https://github.com/enzuru/profiles.git" "/home/enzuru")
 (clone "https://github.com/enzuru/.emacs.d.git" "/home/enzuru/.emacs.d")
+(clone "https://github.com/enzuru/home.git" "/home/enzuru")
+(clone "https://github.com/enzuru/profiles.git" "/home/enzuru/src/profiles")
 (clone "https://github.com/enzuru/notes.git" "/home/enzuru/notes")
