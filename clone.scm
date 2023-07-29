87cc7333120840d1ac4390db1d6e0ff4b897010c
Ahmed Khanzada <me@enzu.ru>
2023-07-29 11:00:54 -0400

    Move notes to src directory

--- clone.scm
+++ clone.scm
@@ -3,4 +3,4 @@
 (clone "https://github.com/enzuru/.emacs.d.git" "/home/enzuru/.emacs.d")
 (clone "https://github.com/enzuru/home.git" "/home/enzuru")
 (clone "https://github.com/enzuru/profiles.git" "/home/enzuru/src/profiles")
-(clone "https://github.com/enzuru/notes.git" "/home/enzuru/notes")
+(clone "https://github.com/enzuru/notes.git" "/home/enzuru/src/notes")
