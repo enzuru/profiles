c07750b0fa12d7d1df544c0563184688457ce722
Ahmed Khanzada <me@enzu.ru>
2023-09-05 14:48:40 -0400

    Make configuration more readable

--- configuration.scm
+++ configuration.scm
@@ -20,37 +20,34 @@
                 (group "users")
                 (home-directory "/home/enzuru")
                 (shell (file-append fish "/bin/fish"))
-                (supplementary-groups
-                 '("wheel" "netdev" "audio" "video" "docker")))
+                (supplementary-groups '("wheel" "netdev" "audio" "video" "docker")))
                %base-user-accounts))
+
  (packages
-  (append
-   (list
-    (specification->package "openbox")
-    (specification->package "fish")
-    (specification->package "nss-certs"))
-   %base-packages))
+  (append (list (specification->package "openbox")
+                (specification->package "fish")
+                (specification->package "nss-certs"))
+          %base-packages))
+
  (services
-  (append
-   (list (service openssh-service-type)
-         (service tor-service-type)
-         (service docker-service-type)
-         (set-xorg-configuration
-          (xorg-configuration
-           (keyboard-layout keyboard-layout))))
+  (append (list (service openssh-service-type)
+                (service tor-service-type)
+                (service docker-service-type)
+                (set-xorg-configuration (xorg-configuration (keyboard-layout keyboard-layout))))
    %desktop-services))
+
  (bootloader
   (bootloader-configuration
    (bootloader grub-bootloader)
    (target "/dev/sda")
    (keyboard-layout keyboard-layout)))
+
  (swap-devices
   (list (uuid "5637957b-2157-44b4-a948-367a25f80743")))
+
  (file-systems
   (cons* (file-system
           (mount-point "/")
-          (device
-           (uuid "495afef9-3f45-4da1-aa4d-7079807b9090"
-                 'ext4))
+          (device (uuid "495afef9-3f45-4da1-aa4d-7079807b9090" 'ext4))
           (type "ext4"))
          %base-file-systems)))
