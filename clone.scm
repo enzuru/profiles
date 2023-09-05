21f0e1ae3c2337de7e0e9ae8f591f7d706ce3a87
Ahmed Khanzada <me@enzu.ru>
2023-09-05 14:45:00 -0400

    Add cloning of Quicklisp projects

--- clone.scm
+++ clone.scm
@@ -1,6 +1,9 @@
 (use-modules (git))
 
-(clone "https://github.com/enzuru/.emacs.d.git" "/home/enzuru/.emacs.d")
-(clone "https://github.com/enzuru/home.git" "/home/enzuru")
-(clone "https://github.com/enzuru/profiles.git" "/home/enzuru/src/profiles")
-(clone "https://github.com/enzuru/notes.git" "/home/enzuru/src/notes")
+(clone "https://github.com/enzuru/.emacs.d.git" "~/.emacs.d")
+(clone "https://github.com/enzuru/home.git" "~/")
+(clone "https://github.com/enzuru/profiles.git" "~/src/profiles")
+(clone "https://github.com/enzuru/notes.git" "~/src/notes")
+
+(clone "https://github.com/stumpwm/stumpwm.git" "~/quicklisp/local-projects/stumpqm")
+(clone "https://github.com/goose121/clx-truetype.git" "~/quicklisp/local-projects/clx-truetype")
