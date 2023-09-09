9ed01b06b9760a97871b78d893d6535f223e0739
enzu.ru <enzuru@users.noreply.github.com>
2023-09-09 10:59:41 -0400

    Better cloning

--- clone.scm
+++ clone.scm
@@ -1,9 +1,10 @@
 (use-modules (git))
 
-(clone "https://github.com/enzuru/.emacs.d.git" "~/.emacs.d")
 (clone "https://github.com/enzuru/home.git" "~/")
+(clone "https://github.com/enzuru/.emacs.d.git" "~/.emacs.d")
 (clone "https://github.com/enzuru/profiles.git" "~/src/profiles")
 (clone "https://github.com/enzuru/notes.git" "~/src/notes")
 
-(clone "https://github.com/stumpwm/stumpwm.git" "~/quicklisp/local-projects/stumpqm")
-(clone "https://github.com/goose121/clx-truetype.git" "~/quicklisp/local-projects/clx-truetype")
+(clone "https://github.com/stumpwm/stumpwm.git" "~/quicklisp/local-projects/stumpwm")
+(clone "https://github.com/stumpwm/clx-truetype.git" "~/quicklisp/local-projects/clx-truetype")
+(clone "https://github.com/stumpwm/stumpwm-contrib.git" "~/.stumpwm.d/contrib")
